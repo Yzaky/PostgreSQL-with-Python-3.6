5bfd96ceab94560a2a3ce45c2ba3755c620407d7
Youssef Zaky <youssefzaky94@hotmail.com>
2017-06-24 21:32:59 -0400

    Mod

--- database/app.py
+++ database/app.py
@@ -1,11 +1,12 @@
 from user import User
+from database import Database
 
-my_user=User('test@test.com','Rolf','Test',None)
+Database.init()
 
-my_user.save_to_db()
+user=User('test@test.com','test1','test2',None)
+
+user.save_to_db()
 
 user_from_db=User.load_from_db_by_email('test@test.com')
 
 print(user_from_db)
-
-my_user.save_to_db()
--- database/database.py
+++ database/database.py
@@ -1,19 +1,50 @@
 from psycopg2 import pool
 
-
-connection_pool=pool.SimpleConnection(1,10,
+class Database:
+    connection_pool=None
+    
+    
+    @classmethod
+    def Init(cls):
+        cls.connection_pool=pool.SimpleConnection(1,10,
                                       database="learning",
                                       user="progres",
                                       host="localhost")
-
-class ConnectionFromPool:
+               
+    @classmethod
+    def getconnection(cls):
+        return cls.connection_pool.getconn()
+        
+    
+    @classmethod
+    def return_connection(cls,connection):
+        Database.connection_pool.putconn(connection)
+        
+    
+    @classmethod
+    def closeall(cls):
+        Database.connection_pool.closeall()
+        
+        
+        
+class CursorFromConnectionPool:
+    connection_pool=None
+    
     def __init__(self):
         self.connection=None
+        self.cursor= None
 
 
     def __enter__(self):
-        self.connection=connection_pool.getconn()
-        return self.connection
+        self.connection=Database.getconnection()
+        self.cursor=self.connection.cursor()
+        return self.cursor
     
-    def __exit__(self, exc_type, exc_val, exc_tb):
-        self.connection_pool.putconn(self.connection)
+    
+    def __exit__(self,exception_type,exception_value,exception_traceback):
+        if exception_value is not None:   # errors like TypeError
+            self.connection.rollback()
+        else:
+            self.cursor.close()
+            self.connection.commit()
+        Database.return_connection(self.connection)
